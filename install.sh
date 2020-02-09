24e0098fd87eb0ec04d390cbd9a5c374e48c9959
imanol <imanol.rodriguez@fhios.es>
2020-02-09 18:56:54 +0100

    best script to install

--- install.sh
+++ install.sh
@@ -10,10 +10,11 @@ source .env
 ./start.sh
 #backend build 
 sleep 5
-docker exec drupalreactjamstack_backend_1 bash -c 'composer install -o'
-docker exec drupalreactjamstack_backend_1 bash -c "vendor/bin/drush site-install --db-url=mysql://root:${MYSQL_ROOT_PASSWORD}@db:3306/${MYSQL_DEFAULT_DATABASE} -y"
-docker exec drupalreactjamstack_backend_1 bash -c "vendor/bin/drush config-set 'system.site' uuid '${DRUPAL_UUID}'"
-docker exec drupalreactjamstack_backend_1 bash -c "vendor/bin/drush cim --partial -y"
+
+docker-compose exec backend bash -c "composer install -o"
+docker-compose exec backend bash -c "vendor/bin/drush site-install --db-url=mysql://root:${MYSQL_ROOT_PASSWORD}@db:3306/${MYSQL_DEFAULT_DATABASE} -y"
+docker-compose exec backend bash -c "vendor/bin/drush config-set 'system.site' uuid '${DRUPAL_UUID}' -y"
+docker-compose exec backend bash -c "vendor/bin/drush cim --partial -y"
 
 #frontend build
 cd frontend
