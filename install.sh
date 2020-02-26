b368b27c490b8c00e77f18d3283f160aa4b964d4
Imanol <imanol.rodriguez@fhios.es>
2020-02-26 15:07:19 +0100

    Comments

--- install.sh
+++ install.sh
@@ -11,13 +11,20 @@ source .env
 #backend build 
 sleep 5
 
+# Install dependencies with composer for Drupal
 docker-compose exec -u 1000 backend bash -c "composer install -o"
+# Install Drupal site
 docker-compose exec -u 1000 backend bash -c "vendor/bin/drush site-install --db-url=mysql://root:${MYSQL_ROOT_PASSWORD}@db:3306/${MYSQL_DEFAULT_DATABASE} -y"
+# Config UUUID of Drupal for allow import from repo config
 docker-compose exec -u 1000 backend bash -c "vendor/bin/drush config-set 'system.site' uuid '${DRUPAL_UUID}' -y"
+# Import default config of repo
 docker-compose exec -u 1000 backend bash -c "vendor/bin/drush cim --partial -y"
+# Copy services.yml for allow request HTTP
 docker-compose exec -u 1000 backend bash -c "cp web/sites/default/default.services.yml web/sites/default/services.yml"
 
 #frontend build
 cd frontend
+# Install dependencies
 npm install
+# Build React app
 npm run build
